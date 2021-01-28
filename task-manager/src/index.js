355eaba8adbeed933eeb25e33bfd96caeade4b95
CalistaIo <e0426281@u.nus.edu>
2021-01-29 00:52:38 +0800

    Create resource endpoints for reading tasks

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -27,6 +27,50 @@ app.post('/tasks', (req, res) => {
     });
 })
 
+// Get all users
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users);
+    }).catch((error) => {
+        res.status(500).send();
+    });
+});
+
+// Get single user by id
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id;
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    }).catch((error) => {
+        res.status(500).send();
+    });
+});
+
+// Get all tasks
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks);
+    }).catch((error) => {
+        res.status(500).send();
+    });
+});
+
+// Get single task by id
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id;
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    }).catch((error) => {
+        res.status(500).send();
+    });
+});
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
 })
