e75e2b51f3c8f504c15aa382ad9dee966863b2c3
CalistaIo <e0426281@u.nus.edu>
2021-02-02 00:30:16 +0800

    Set up routers for users and tasks

---
+++ task-manager/src/routers/task.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const Task = require('../models/task.js');
+const router = new express.Router();
+
+// Create task
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body);
+    try {
+        await task.save();
+        res.status(201).send(task);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+    // task.save().then(() => {
+    //     res.status(201).send(task);
+    // }).catch((error) => {
+    //     res.status(400).send(error);
+    // });
+});
+
+// Get all tasks
+router.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.send(tasks);
+    } catch (e) {
+        res.status(500).send();
+    }
+    // Task.find({}).then((tasks) => {
+    //     res.send(tasks);
+    // }).catch((error) => {
+    //     res.status(500).send();
+    // });
+});
+
+// Get single task by id
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+    try {
+        const task = await Task.findById(_id);
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    } catch (e) {
+        res.status(500).send();
+    }
+    // Task.findById(_id).then((task) => {
+    //     if (!task) {
+    //         return res.status(404).send();
+    //     }
+    //     res.send(task);
+    // }).catch((error) => {
+    //     res.status(500).send();
+    // });
+});
+
+// Update individual task
+router.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['description', 'completed'];
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update);
+    });
+    if (!isValidOperation) {
+        return res.status(400).send({error: 'Invalid Updates!'});
+    }
+
+    try {
+        const _id = req.params.id;
+        const task = await Task.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true});
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+router.delete('/tasks/:id', async (req, res) => {
+    try {
+        const _id = req.params.id;
+        const task = await Task.findByIdAndDelete(_id);
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
+module.exports = router;
